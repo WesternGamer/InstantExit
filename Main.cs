8f9d0970ae5078195e941dd5b93165beb935dfb0
WesternGamer <kevnguyen474@gmail.com>
2021-09-22 18:51:33 -0400

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using VRage.Plugins;
+
+namespace InstantExit
+{
+    public class Main : IPlugin
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Init(object gameInstance)
+        {
+            Harmony harmony = new Harmony("InstantExit");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
---
+++ Utill/Patches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Sandbox;
+using System.Diagnostics;
+
+namespace InstantExit.Utill
+{
+    [HarmonyPatch(typeof(MySandboxGame), "ExitThreadSafe")]
+    public class Patch_ExitThreadSafe
+    {
+        public static bool Prefix()
+        {
+            Process.GetCurrentProcess().Kill();
+            return false;
+        }
+    }
+}
